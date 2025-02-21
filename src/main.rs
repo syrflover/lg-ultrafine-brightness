7bc0684284cb71d8d2874bfee51014f771d0eff3
syr <user@syrflover.co>
2025-02-21 14:54:05 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+const VENDOR_ID: u16 = 0x43e;
+const PRODUCT_ID: u16 = 0x9a40;
+const MAX_BRIGHTNESS: u16 = 0xd2f0;
+const MIN_BRIGHTNESS: u16 = 0x0190;
+
+fn main() {
+    println!("Hello, world!");
+}
